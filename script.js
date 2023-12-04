792fc77322410f9d199a612fc790691cb5f91fba
Vanderson Nascimento <vando.nsc@gmail.com>
2023-12-04 14:48:11 -0300

    Colocado os valores em json no corpo do site e finalizado o projeto

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 async function buscarEndereco(cep){
+    const mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try{
        const consultaCEP= await fetch(`https://viacep.com.br/ws/${cep}/json`)
         const consultaCEPConvertida = await consultaCEP.json()
         if(consultaCEPConvertida.erro){
             throw Error('CEP não existente')
         }
+        const cidade = document.getElementById('cidade');
+        const rua = document.getElementById('endereco');
+        const estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        rua.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida
     } catch(erro){
         console.log(erro)
+        mensagemErro.innerHTML = `<p>CEP invalido. tente novamente!</p>`
     }
     
 }
-
+const cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscarEndereco(cep.value))
 buscarEndereco();
